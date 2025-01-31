72ccf742604715ebe89f8b863e7e99e31284cf67
JessCP25 <jpayanoc25@gmail.com>
2025-01-30 23:25:03 -0500

    Feat: Maquetación del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./components/products/products.component').then(
+        (c) => c.ProductsComponent
+      ),
+  },
+  {
+    path: 'pico-preview',
+    loadComponent: () =>
+      import('./components/pico-preview/pico-preview.component').then(
+        (c) => c.PicoPreviewComponent
+      ),
+  },
+];
