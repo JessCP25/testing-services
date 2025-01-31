f92efbe37e52a1a09c25e65672871bf2020bd7a6
JessCP25 <jpayanoc25@gmail.com>
2025-01-30 23:54:07 -0500

    Feat: Product service http

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { ProductsService } from '../../services/product.service';
 
 @Component({
   selector: 'app-products',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductsService){}
+
+  ngOnInit(): void {
+      this.getProduct();
+  }
+
+  getProduct(){
+    this.productService.getAllSimple().subscribe({
+      next: (res)=> {
+        if(res){
+          this.products = res;
+        }
+      },
+      error: (err) => {}
+    })
+  }
 }
