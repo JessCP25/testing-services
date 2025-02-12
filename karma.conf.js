720158366072301f992b135908a5a78428bf5ef2
JessCP25 <jpayanoc25@gmail.com>
2025-02-11 23:24:30 -0500

    Feat: GitHub actions

--- karma.conf.js
+++ karma.conf.js
@@ -33,15 +33,21 @@ module.exports = function (config) {
       ],
       check: {
         global: {
-          statements: 80,
-          branches: 80,
-          functions: 80,
-          lines: 80
+          statements: 30,
+          branches: 30,
+          functions: 30,
+          lines: 30
         }
       }
     },
     reporters: ['mocha'],
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadLessCI:{
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     restartOnFileChange: true
   });
 };
