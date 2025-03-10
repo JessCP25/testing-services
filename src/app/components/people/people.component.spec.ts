4d77ba3c52764b38ca7e1a6472df65f88337fb6a
JessCP25 <jpayanoc25@gmail.com>
2025-03-09 20:00:24 -0500

    Feat: Resolviendo reto

--- src/app/components/people/people.component.spec.ts
+++ src/app/components/people/people.component.spec.ts
@@ -1,17 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PeopleComponent } from './people.component';
+import { PersonComponent } from '../person/person.component';
+import { Person } from '../../models/person.model';
+import { By } from '@angular/platform-browser';
 
-describe('PeopleComponent', () => {
+fdescribe('PeopleComponent', () => {
   let component: PeopleComponent;
   let fixture: ComponentFixture<PeopleComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PeopleComponent]
-    })
-    .compileComponents();
-    
+      imports: [PeopleComponent, PersonComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PeopleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +22,39 @@ describe('PeopleComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render people.length component person', () => {
+    // Arrange
+    component.people = [
+      new Person('Jessica', 'Payano', 25, 1, 1),
+      new Person('Silvia', 'Payano', 25, 1, 1),
+      new Person('Camila', 'Payano', 25, 1, 1),
+    ];
+    // Act
+    fixture.detectChanges();
+    const personDegug = fixture.debugElement.queryAll(By.css('app-person'));
+    // Assert
+    expect(personDegug.length).toEqual(3);
+  });
+
+  it('should render the person with click and show selected person', ()=>{
+    // Arrange
+    const buttonDebug = fixture.debugElement.queryAll(By.css('app-person .button-choose'));
+    // Act
+    buttonDebug.forEach((btn, index) => {
+      btn.triggerEventHandler('click', null);
+      fixture.detectChanges();
+      const selectedPerson = component.selectedPerson;
+
+      const nameDebug = fixture.debugElement.query(By.css('.name'));
+      const nameElement = nameDebug.nativeElement;
+      const ageDebug = fixture.debugElement.query(By.css('.age'));
+      const ageElement = ageDebug.nativeElement;
+      // Assert
+      expect(nameElement.textContent).toContain(selectedPerson?.name)
+      expect(ageElement.textContent).toContain(selectedPerson?.age)
+
+      expect(selectedPerson).toEqual(component.people[index]);
+    })
+  })
 });
--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { PersonComponent } from '../person/person.component';
 import { Person } from '../../models/person.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-people',
   standalone: true,
-  imports: [PersonComponent],
+  imports: [PersonComponent, CommonModule],
   templateUrl: './people.component.html',
-  styleUrl: './people.component.scss'
+  styleUrl: './people.component.scss',
 })
 export class PeopleComponent {
-  person = new Person('Jessica', 'Payano', 25, 1,1)
+  people: Person[] = [
+    new Person('Jessica', 'Payano', 25, 1, 1),
+    new Person('Silvia', 'Payano', 25, 1, 1),
+  ];
+
+  selectedPerson: Person | null = null;
+
+  choose(person: Person){
+    this.selectedPerson = person;
+  }
 }
